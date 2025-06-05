1875aff1e42164fdea744b78144b377222d11e19
Jakub Stępień <devjakub.stepien@gmail.com>
2025-06-05 23:22:52 +0200

    feat: convert useWasm hook to ts

--- client/src/hooks/useWasm.ts
+++ client/src/hooks/useWasm.ts
@@ -2,15 +2,15 @@ import { useEffect, useState } from "preact/hooks";
 import init from "../wasm/wasm_api.js";
 
 export function useWasm() {
-  const [wasmReady, setWasmReady] = useState(false);
-  const [wasmError, setWasmError] = useState(null);
+  const [wasmReady, setWasmReady] = useState<boolean>(false);
+  const [wasmError, setWasmError] = useState<Error | null>(null);
 
   useEffect(() => {
     const loadWasm = async () => {
       try {
         await init();
         setWasmReady(true);
-      } catch (error) {
+      } catch (error: any) {
         setWasmError(error);
       }
     };
