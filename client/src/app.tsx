435b2abfb4ae1ef9a1d98ab19da47541306ec747
kacienk <ktcienkosz@gmail.com>
2025-06-07 20:44:21 +0200

    feat: create algorithms bar

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,17 +1,17 @@
-import ImageConverter from "@/components/images/ImageConverter.jsx";
-import { useWasm } from "./hooks/useWasm.js";
+import ImageConverter from '@/components/images/ImageConverter';
+import { useWasm } from '@/hooks/useWasm';
 
 export function App() {
   const { wasmReady, wasmError } = useWasm();
-  console.log("WASM ready:", wasmReady);
+  console.log('WASM ready:', wasmReady);
 
   return (
-      <div className="flex flex-col h-screen p-2">
-        {wasmError && <p>Error initializing WASM: {wasmError.message}</p>}
-        <div className="w-full bg-orange-300 flex items-center justify-start text-3xl font-bold rounded-md">
-          <div className='px-6 py-2'>Rustoscope</div>
-        </div>
-         <ImageConverter />
+    <div className="flex flex-col h-screen p-2">
+      {wasmError && <p>Error initializing WASM: {wasmError.message}</p>}
+      <div className="w-full bg-orange-300 flex items-center justify-start text-3xl font-bold rounded-md">
+        <div className="px-6 py-2">Rustoscope</div>
       </div>
+      <ImageConverter />
+    </div>
   );
 }
